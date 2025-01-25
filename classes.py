9b852cc3e1cfaa4ad187f4d5703a07a574662b36
Pablo Hueso <pablohuesomerino@gmail.com>
2025-01-25 17:45:49 +0100

    define linear nn class

---
+++ classes.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+
+#################################################################################################
+# Here we define the neural network classes that are presented in sections 2 and 3 of the paper #
+#################################################################################################
+
+# Deep linear network (section 2)
+class LinearNN(nn.Module):
+    def __init__(self, dim_x, dim_y, *hidden_dims):
+        """
+        Linear Neural Network. Uses no activation functions and allows for variable widths in all layers.
+        
+        Parameters:
+        - dim_x (int): Input dimension.
+        - dim_y (int): Output dimension.
+        - hidden_dims (list of int): Dimensions of hidden layers.
+        """
+        super().__init__()
+        
+        self.dim_x = dim_x
+        self.dim_y = dim_y
+        self.hidden_dims = hidden_dims
+        self.H = len(hidden_dims)  
+
+        layers = []
+        dims = [dim_x] + list(hidden_dims) + [dim_y]  
+        
+        for i in range(len(dims) - 1):
+            layers.append(nn.Linear(dims[i], dims[i + 1], bias=False))
+        
+        self.layers = nn.ModuleList(layers)
+
+    def forward(self, x):
+        """
+        Forward pass through the linear neural network.
+        """
+        for layer in self.layers:
+            x = layer(x)  
+        return x
+
