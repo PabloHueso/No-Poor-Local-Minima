3dd914b998eca76b272352a2baad9379f71be63c
Pablo Hueso <pablohuesomerino@gmail.com>
2025-01-27 16:37:44 +0100

    add comment with partial implementation of non linear network

--- classes.py
+++ classes.py
@@ -9,7 +9,7 @@ import torch.nn as nn
 class LinearNN(nn.Module):
     def __init__(self, dim_x, dim_y, *hidden_dims):
         """
-        Linear Neural Network. Uses no activation functions and allows for variable widths in all layers.
+        Deep Linear Neural Network. Uses no activation functions and allows for variable widths in all layers.
         
         Parameters:
         - dim_x (int): Input dimension.
@@ -39,3 +39,41 @@ class LinearNN(nn.Module):
             x = layer(x)  
         return x
 
+'''
+# Deep nonlinear network (section 3)
+class NonlinearNN(nn.Module):
+    def __init__(self, dim_x, dim_y, *hidden_dims):
+        """
+        Deep Nonlinear Neural Network. Uses the ReLU activation function and allows for variable widths in all layers.
+        
+        Parameters:
+        - dim_x (int): Input dimension.
+        - dim_y (int): Output dimension.
+        - hidden_dims (list of int): Dimensions of hidden layers.
+        """
+        super().__init__()
+        
+        self.dim_x = dim_x
+        self.dim_y = dim_y
+        self.hidden_dims = hidden_dims
+        self.H = len(hidden_dims)  
+        #self.q = 1 / (p) mirar la def de p en el paper.
+
+        layers = []
+        dims = [dim_x] + list(hidden_dims) + [dim_y]  
+        
+        for i in range(len(dims) - 1):
+            layers.append(nn.Linear(dims[i], dims[i + 1], bias=False))
+        
+        self.layers = nn.ModuleList(layers)
+
+    def forward(self, x):
+        """
+        Forward pass through the linear neural network.
+        """
+        for layer in self.layers:
+            x = layer(x) 
+            x =  torch.nn.functional.relu(x)
+        #x = self.q * x
+        return x
+'''
